957681c40ac2d96157ef3dcd7ed3cbe562e45c8d
Bella Ranalli <bela.ranalli@gmail.com>
2022-09-11 21:21:03 -0300

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hola bbito te super quiero</p>
+        <p>probando app</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
